fc481f4992ef57ca71d25d94b647092b93f981b9
NatanaelNetto <natanaelfelix0@gmail.com>
2025-05-16 20:36:57 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
-
-const circulo = document.querySelector(".circulo");
-
 function trocarCor(cor) {
-    circulo.style.background = cor;
+    document.querySelector(".circulo").style.background = cor;
+    document.querySelector(".starbucks").src = `./img/img${["#017143", "#eb7495", "#d752b1"].indexOf(cor) + 1}.png`;
 }
